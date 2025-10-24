fb915638522c74d61442f0c6fd4f6020ad49fb74
DIMPLE <Ddimple@rrc.ca>
2025-10-23 19:19:01 -0500

    Feat: Did all the required update for the Ticket 2 business requirements.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import morgan from "morgan";
+
 import moderationRoutes from "./api/v1/routes/moderationRoutes";
 
 const app: Express = express();
-app.use(express.json());
+
+//Environment based configuration
+const NODE_ENV = process.env.NODE_ENV || "development";
+const LOG_LEVEL = process.env.LOG_LEVEL || "info";
+
+// Use morgan for HTTP request logging
+app.use(morgan("development"));
 
 /**
  * Mount moderation routes on /api/v1/moderation
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import app from "./app";
 import { Server } from "http";
+import dotenv from "dotenv";
+dotenv.config();
 
+//Fallback values for variables
 const PORT: string | number = process.env.PORT || 3000;
+const NODE_ENV: string = process.env.NODE_ENV || "development";
+const LOG_LEVEL: string = process.env.LOG_LEVEL || "info";
 
 const server: Server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`Environment:${NODE_ENV}`);
+    console.log(`Log level: ${LOG_LEVEL}`);
 });
 
 export default server;
